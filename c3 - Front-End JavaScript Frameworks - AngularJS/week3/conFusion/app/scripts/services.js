a139b925f880a438cb85993b1a23c2c46c0afb70
Hefang Li <hefangli0412@gmail.com>
2015-12-15 20:11:35 -0800

    using service instead of factory

--- c3 - Front-End JavaScript Frameworks - AngularJS/week3/conFusion/app/scripts/services.js
+++ c3 - Front-End JavaScript Frameworks - AngularJS/week3/conFusion/app/scripts/services.js
@@ -1,9 +1,7 @@
 'use strict';
 
 angular.module('confusionApp')
-        .factory('menuFactory', function() {
-
-            var menufac = {};
+        .service('menuFactory', function() {
     
             var dishes=[
                          {
@@ -165,13 +163,12 @@ angular.module('confusionApp')
                         }
                         ];
     
-                menufac.getDishes = function(){
+                this.getDishes = function(){
                     return dishes;
                 };
     
-                menufac.getDish = function (index) {
+                this.getDish = function (index) {
                     return dishes[index];
                 };
     
-            return menufac;
         });
