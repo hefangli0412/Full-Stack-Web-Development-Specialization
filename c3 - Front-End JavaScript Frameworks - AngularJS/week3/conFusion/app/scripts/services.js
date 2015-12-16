3e62459d755a99bd7084578d2bdf0ae13a4d06c4
Hefang Li <hefangli0412@gmail.com>
2015-12-15 22:54:03 -0800

    angular ngroute

--- c3 - Front-End JavaScript Frameworks - AngularJS/week3/conFusion/app/scripts/services.js
+++ c3 - Front-End JavaScript Frameworks - AngularJS/week3/conFusion/app/scripts/services.js
@@ -5,13 +5,14 @@ angular.module('confusionApp')
     
             var dishes=[
                          {
+                          _id:0,
                           name:'Uthapizza',
                           image: 'images/uthapizza.png',
                           category: 'mains',
-                           label:'Hot',
+                          label:'Hot',
                           price:'4.99',
                           description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comments: [
+                          comments: [
                                {
                                    rating:5,
                                    comment:"Imagine all the eatables, living in conFusion!",
@@ -44,10 +45,11 @@ angular.module('confusionApp')
                                }                                                          ]
                         },
                         {
+                          _id:1,
                           name:'Zucchipakoda',
-                           image: 'images/zucchipakoda.png',
+                          image: 'images/zucchipakoda.png',
                           category: 'appetizer',
-                           label:'',
+                          label:'',
                           price:'1.99',
                           description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
                           comments: [
@@ -83,13 +85,14 @@ angular.module('confusionApp')
                                }                                                          ]
                         },
                         {
+                          _id:2,
                           name:'Vadonut',
-                           image: 'images/vadonut.png',
+                          image: 'images/vadonut.png',
                           category: 'appetizer',
-                           label:'New',
+                          label:'New',
                           price:'1.99',
                           description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-                           comments: [
+                          comments: [
                                {
                                    rating:5,
                                    comment:"Imagine all the eatables, living in conFusion!",
@@ -123,13 +126,14 @@ angular.module('confusionApp')
                                                           ]
                         },
                         {
+                          _id:3,
                           name:'ElaiCheese Cake',
-                           image: 'images/elaicheesecake.png',
+                          image: 'images/elaicheesecake.png',
                           category: 'dessert',
-                           label:'',
+                          label:'',
                           price:'2.99',
                           description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-                           comments: [
+                          comments: [
                                {
                                    rating:5,
                                    comment:"Imagine all the eatables, living in conFusion!",
